aba5277a56e9c6ad84ab988c05f687589cc25ce0
Abinayaveeraiyan <202100909@sigc.edu>
2024-03-11 04:05:02 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>RELIGIOUS ORGANIZATION</h3>
+      <img className="home-img" src="https://blog.yatradham.org/wp-content/uploads/2021/08/Pazhamudircholai-Murugan-Temple-Timings.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
